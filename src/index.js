3ca395d093807a3b4328d3f252bfb8107fc1b6d6
Gabriel Guimaraes <gabriellimaguimaraes@gmail.com>
2017-11-22 12:26:14 -0500

    New create-react-app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './main';
+import App from './pagedraw/main';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
